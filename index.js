d618b8ad41c40a5031e18ec44a6f6e0436681c33
Hans Naert <hans.naert@vives.be>
2022-12-09 12:38:13 +0100

    add browsermqtt and use it in index.html

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 
-app.use(express.static('public'))
+app.get('/browserMqtt.js',(req,res) => {
+    console.log("browserMqtt.js requested");
+    return res.sendFile(__dirname + '/node_modules/mqtt/dist/mqtt.js');
+});
+app.use(express.static('public'));
 
 server.listen(3000, () => {
   console.log('listening on *:3000');
